ba689ad894be88294fc34f8fd7e1085df8ca6b43
Milena ManojloviÄić <johndoe@example.com>
2022-12-18 12:45:32 +0100

    Domaći 16.12.2022.

---
+++ src/main/java/d_16_12_2022/Zadatak2.java
@@ -0,0 +1,28 @@
+package d_16_12_2022;
+
+import java.util.Scanner;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+
+ // 1.	2.	Napisati program koji stampa 25 _ (donjih crta),
+        // pritom na svaku 5. iteraciju odstampa i novi red.
+        // Zadatak resiti pomocu FOR petlje.
+        //Primer izvrsenja:
+        //_ _ _ _ _
+        //_ _ _ _ _
+        //_ _ _ _ _
+        //_ _ _ _ _
+        //_ _ _ _ _
+
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println ("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+
+        }
+
+        }
+
+    }
+
